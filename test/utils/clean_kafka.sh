88ed37fabb83574cead471eafe82cf7a6f5612e9
Petr Pchelko <ppchelko@wikimedia.org>
2016-04-15 09:30:08 -0700

    Don't attempt to delete topic one more time

--- test/utils/clean_kafka.sh
+++ test/utils/clean_kafka.sh
@@ -5,7 +5,10 @@ dropTopics ( ) {
   then
     PATTERN=$1
     echo "looking for topics named '*${PATTERN}*'..."
-    for TOPIC in `${KAFKA_HOME}/bin/kafka-topics.sh --zookeeper localhost:2181 --list | grep ${PATTERN}`
+    TOPICS=`${KAFKA_HOME}/bin/kafka-topics.sh --zookeeper localhost:2181 --list \
+    	| grep ${PATTERN} \
+    	| grep -v 'marked for deletion$'`
+    for TOPIC in ${TOPICS}
     do
       echo "dropping topic ${TOPIC}"
       ${KAFKA_HOME}/bin/kafka-topics.sh --zookeeper localhost:2181 --delete --topic ${TOPIC} > /dev/null
