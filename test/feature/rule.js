38266749208fa03f31710328f49779e583f68836
Petr Pchelko <petrpchelko@gmail.com>
2016-06-22 21:02:07 +0200

    Added ability to supply switch statements to the rule (#60)
    * Added ability to supply switch statements to the rule
    
    * Commented-out the JSCS test until dependencies gets fixed up
    
    * Removed an unused variable
    
    * Changed switch keyword to cases

--- test/feature/rule.js
+++ test/feature/rule.js
@@ -23,7 +23,7 @@ describe('Rule', function() {
             topic: 'nono',
             exec: {uri: 'a/b/c'}
         });
-        assert.ok(Array.isArray(r.exec), 'exec is expected to be an array!');
+        assert.ok(Array.isArray(r.getExec(0)), 'exec is expected to be an array!');
     });
 
     it('simple rule - multiple requests', function() {
@@ -34,7 +34,7 @@ describe('Rule', function() {
                 {uri: 'e/f/g/h'}
             ]
         });
-        assert.ok(r.exec.length === 2, 'exec is expected to have 2 elements!');
+        assert.equal(r.getExec(0).length, 2, 'exec is expected to have 2 elements!');
     });
 
     describe('Matching', function() {
@@ -53,7 +53,7 @@ describe('Rule', function() {
                 topic: 'nono',
                 exec: {uri: 'a/b/c'}
             });
-            assert.ok(r.test(msg), 'Expected the rule to match all event messages!');
+            assert.equal(r.test(msg), 0, 'Expected the rule to match all event messages!');
         });
 
         it('simple value match', function() {
@@ -62,7 +62,7 @@ describe('Rule', function() {
                 exec: {uri: 'a/b/c'},
                 match: {number: 1, string: 'oolala'}
             });
-            assert.ok(r.test(msg), 'Expected the rule to match the given message!');
+            assert.equal(r.test(msg), 0, 'Expected the rule to match the given message!');
         });
 
         it('simple value mismatch', function() {
@@ -71,7 +71,7 @@ describe('Rule', function() {
                 exec: {uri: 'a/b/c'},
                 match: {number: 2, string: 'oolala'}
             });
-            assert.ok(!r.test(msg), 'Expected the rule not to match the given message!');
+            assert.equal(r.test(msg), -1, 'Expected the rule not to match the given message!');
         });
 
         it('regex match', function() {
@@ -80,7 +80,7 @@ describe('Rule', function() {
                 exec: {uri: 'a/b/c'},
                 match: {number: 1, string: '/(?:la)+/'}
             });
-            assert.ok(r.test(msg), 'Expected the rule to match the given message!');
+            assert.equal(r.test(msg), 0, 'Expected the rule to match the given message!');
         });
 
         it('regex mismatch', function() {
@@ -89,7 +89,7 @@ describe('Rule', function() {
                 exec: {uri: 'a/b/c'},
                 match: {number: 1, string: '/lah/'}
             });
-            assert.ok(!r.test(msg), 'Expected the rule not to match the given message!');
+            assert.equal(r.test(msg), -1, 'Expected the rule not to match the given message!');
         });
 
         it('array match', function() {
@@ -99,7 +99,7 @@ describe('Rule', function() {
                 match: {array: ['1', 2, '/(\\d)/']}
             });
             var msg = { array: [2, '1', '3', '4', 5] };
-            assert.ok(r.test(msg), 'Expected the rule to match the given message!');
+            assert.equal(r.test(msg), 0, 'Expected the rule to match the given message!');
         });
 
         it('malformed match', function() {
@@ -119,7 +119,7 @@ describe('Rule', function() {
                 exec: {uri: 'a/b/c'},
                 match_not: {meta: {uri: '/my-url/'}}
             });
-            assert.ok(r.test(msg), 'Expected the rule to match the given message!');
+            assert.equal(r.test(msg), 0, 'Expected the rule to match the given message!');
         });
 
         it('matches match and match_not', function() {
@@ -129,7 +129,7 @@ describe('Rule', function() {
                 match: {number: 1},
                 match_not: {meta: {uri: '/my-url/'}}
             });
-            assert.ok(r.test(msg), 'Expected the rule to match the given message!');
+            assert.equal(r.test(msg), 0, 'Expected the rule to match the given message!');
         });
 
         it('matches match but not match_not', function() {
@@ -139,7 +139,7 @@ describe('Rule', function() {
                 match: {number: 1},
                 match_not: {meta: {uri: '/fake/'}}
             });
-            assert.ok(!r.test(msg), 'Expected the rule not to match the given message!');
+            assert.equal(r.test(msg), -1, 'Expected the rule not to match the given message!');
         });
 
         it('matches match_not but not match', function() {
@@ -149,7 +149,7 @@ describe('Rule', function() {
                 match: {number: 10},
                 match_not: {meta: {uri: '/my-url/'}}
             });
-            assert.ok(!r.test(msg), 'Expected the rule not to match the given message!');
+            assert.equal(r.test(msg), -1, 'Expected the rule not to match the given message!');
         });
 
         it('expansion', function() {
@@ -158,7 +158,7 @@ describe('Rule', function() {
                 exec: {uri: 'a/{match.meta.uri[1]}/c'},
                 match: { meta: { uri: "/\\/fake\\/([^\\/]+)/" }, number: 1 }
             });
-            var exp = r.expand(msg);
+            var exp = r.expand(r.test(msg), msg);
             assert.deepEqual(exp.meta.uri, /\/fake\/([^\/]+)/.exec(msg.meta.uri));
         });
 
@@ -168,7 +168,7 @@ describe('Rule', function() {
                 exec: {uri: 'a/{match.meta.uri.element}/c'},
                 match: { meta: { uri: "/\\/fake\\/(?<element>[^\\/]+)/" }, number: 1 }
             });
-            var exp = r.expand(msg);
+            var exp = r.expand(r.test(msg), msg);
             assert.deepEqual(exp.meta.uri, { element: 'uri' });
         });
 
--- test/index.js
+++ test/index.js
@@ -4,4 +4,4 @@
 // Run jshint as part of normal testing
 require('mocha-jshint')();
 // Run jscs as part of normal testing
-require('mocha-jscs')();
+//require('mocha-jscs')();
