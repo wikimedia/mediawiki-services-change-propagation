21e85b41773dd084171505ffdccefe53c7fc18c2
Petr Pchelko <ppchelko@wikimedia.org>
2017-08-29 14:48:28 -0700

    Added proper imports to rule executor

--- test/utils/clean_kafka.sh
+++ test/utils/clean_kafka.sh
@@ -47,6 +47,4 @@ createTopic "test_dc.change-prop.retry.change-prop.wikidata.resource-change"
 wait
 sleep 5
 
-if ! [[ `redis-cli --raw keys "CP*" | xargs redis-cli del` ]]; then
-    echo "WARNING: Couldn't clear redis"
-fi
+redis-cli --raw keys "CP*" | xargs redis-cli del
