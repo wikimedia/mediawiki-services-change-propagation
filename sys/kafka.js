06c42926356b05c739462df57bc02b9b726e499f
Aaron Schulz <aschulz@wikimedia.org>
2025-03-03 12:25:11 -0800

    Fix maximum line length eslint warnings
    Change-Id: I296d4528e98e1d6909ab831a63dc8b4b23d56a26

--- sys/kafka.js
+++ sys/kafka.js
@@ -38,7 +38,9 @@ class Kafka {
     }
 
     _subscribeRules(hyper, rules) {
-        return P.map(Object.keys(rules), ruleName => this.subscriber.subscribe(hyper, ruleName, rules[ruleName]))
+        return P.map(
+            Object.keys(rules),
+            ruleName => this.subscriber.subscribe(hyper, ruleName, rules[ruleName]))
         .thenReturn({ status: 201 });
     }
 
